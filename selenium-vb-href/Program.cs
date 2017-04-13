3e69efe2c21416679d3826f3b984267cfd2adc72
Alifian Mahardhika <fiansmamda@gmail.com>
2017-04-13 16:53:30 +0900

    VB C# Console <href> extractor
    This console application using http://www.seleniumhq.org/ selenium webdriver
    installed via https://www.nuget.org/ nuget package manager on visual basic

---
+++ selenium-vb-href/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace seleniumvbhref
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var driver = new FirefoxDriver();
+            int i = 0;
+            string myurl = "http://www.ncjpn.com/";
+            string filelocation = "E:/Link.txt";
+            driver.Navigate().GoToUrl(myurl);
+            Console.WriteLine("Links found: ");
+            foreach (var item in driver.FindElements(By.TagName("a")))
+            {
+                Console.WriteLine(item.GetAttribute("href"));
+                i++;
+            }
+            string[] lines = new string[i];
+            int j = 0;
+            foreach (var item in driver.FindElements(By.TagName("a")))
+            {
+                lines[j] = item.GetAttribute("href");
+                j++;
+            }
+            File.WriteAllLines(@filelocation, lines);
+            Console.WriteLine("<href> value written on {0}", filelocation);
+            Console.WriteLine("Total Links Found on {0}",myurl, "=");
+            Console.WriteLine("{0}", i, "links");
+            Console.WriteLine("Done");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
